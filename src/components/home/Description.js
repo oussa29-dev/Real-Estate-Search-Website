9238a1583c6328a3668e5d4a1bf301ae429f7ba9
oussa29-dev <ko_benkhaoua@esi.dz>
2024-10-28 13:30:05 +0100

    elemdani2

--- src/components/home/Description.js
+++ src/components/home/Description.js
@@ -2,16 +2,16 @@ const Description = () => {
   return (
     <div className=" mt-28  ">
       <h2 className="text-primary-100 text-[35px] sm:text-[44px] md:text-[52px] lg:text-[64px] font-bold leading-[45px] sm:leading-[59px] md:leading-[70px] lg:leading-[80px] text-left">
-      Trouver <br /> la propriété <br /> la plus adéquate
+      Découvrez<br /> l’appareil le plus<br /> adapté à vos besoins
       </h2>
       <div className="text-[19px] sm:text-[25px] md:text-[28px] lg:text-[36px] font-semibold 
       leading-[34px] sm:leading-[44px] md:leading-[55px] lg:leading-[72px] text-left text-fourth-100">
-        <p>Découvrez l’appareil le plus adapté à vos besoins
-        <br />
+        <p>
+     
         Parcourez une vaste sélection d’appareils électroménagers et trouvez celui qui répond le mieux à vos attentes de manière simple, rapide et efficace.</p>
       </div>
 
-      <button className=" text-[19] sm:text-[21] md:text-[22] lg:text-[24] leading-[36px] sm:leading-[42px] md:leading-[45px] lg:leading-[48px]  font-semibold  text-primary-100 cursor-pointer bg-third-100 rounded-2xl w-48 h-14 px-5 py-1 custom-custom-shadow">
+      <button className=" text-[19px] sm:text-[21px] md:text-[22px] lg:text-[25px] leading-[36px] sm:leading-[42px] md:leading-[45px] lg:leading-[60px]  font-semibold  text-primary-100 cursor-pointer bg-third-100 rounded-2xl w-48 h-14 px-5  custom-custom-shadow">
         Se connecter
       </button>
     </div>
